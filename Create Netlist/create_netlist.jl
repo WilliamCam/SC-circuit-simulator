42b2fda15234e28ad5c8134c92f076512c8cb2f3
kristijan-korunoski <93856849+kristijan-korunoski@users.noreply.github.com>
2021-12-15 15:17:37 +0800

    Most important data is now written to a .jld2 file
    Most important data is written into "test.jld2" - matrices L, σA, σB, k are all written.
    
    There is also another julia program "load_data.jl" which loads data from "test.jld2" and displays matrices L, σA, σB, k.
    
    Note - σA and σB direction is not implemented yet

--- Create Netlist/create_netlist.jl
+++ Create Netlist/create_netlist.jl
@@ -1,21 +1,20 @@
-function netlist(name)                  #USE JLD2.jl
-    nl = open("$(name)_nl.csv", "w")            #Create file containing TOPOLOGY PARAMETERS OF SQUID APF CIRCUIT
-    cp = open("$(name)_cp.txt", "w")            #Create file containing COMPONENT PARAMETERS OF SQUID APF CIRCUIT PARAMETER
-    lc = open("$(name)_lc.csv", "w")            #Create file containing LOOPS AND COMPONENTS CONTAINED IN EACH LOOP
+using JLD2, FileIO
 
-    write(nl, "NetList 1")                      #Title for netlist
-
-    loops = []                                  #Stores components in each loop as array
-    componentLoopDict = Dict()                  #Dictionary with components as keys and loops as values
-    componentParamDict = Dict()                 #Dictionary with components as keys and parameters as values
-    squidLoops = []                             #Maybe uneccessary 
-    mutualInd = []                              #Stores data regarding which loops are mutally coupled
+function netlist(name)
+    loops = []                                  #Stores components in each loop as array of array (MATRIX) 🟢
+    componentLoopDict = Dict()                  #Dictionary with components as keys and loops as values (used to find unique elements)
+    componentParamDict = Dict()                 #Dictionary with components as keys and parameters as values 🟢
+    squidLoops = []                             #Maybe uneccessary 🔴
+    mutualInd = []                              #Stores data regarding which loops are mutally coupled 🟢
     junctions = []                              #Stores the names of the junctions
-    extern_flux = []                            #Stores loops which have external flux
+    extern_flux = []                            #Stores loops which have external flux 🟢
 
     println("Enter the number of loops in the circuit:")
     numLoops = readline()
     numLoops = parse(Int8, numLoops)             #Store number of Loops as an int
+    
+    L = zeros(Float64, 0, numLoops-1)            #🟢 
+    σB = zeros(Float64, 0, numLoops)            #🟢 
 
     println("Enter the Junctions:\n(Enter '~' when all are listed)")
     while true
@@ -26,42 +25,42 @@ function netlist(name)                  #USE JLD2.jl
         push!(junctions, input)
     end
 
-    println("Enter any mutually coupled loops:\nE.g. if loop 1 and 2 are coupled with mutual inductance 5μA/Φ𝜊 enter\n'1,2;5'\n(Enter '~' when all are listed)")
+    println("Enter any mutually coupled loops:\nE.g. if loop 1 and 2 are coupled with mutual inductance 5μA/Φ𝜊 enter\n'1,2,5'\n(Enter '~' when all are listed)")
     while true
         input = readline()
         if (input == "~")           
             break
         end
-        currentMutual = (parse(Int8, input[1]), parse(Int8, input[3]))    #Create tuple ### THIS DOESNT WORK FOR DECIMALS
-        currentMutual = (currentMutual, parse(Int8,input[5]))            #Add tuple to tuple and add value
-        push!(mutualInd, currentMutual)
+        currentMutual = split(input, ',')
+        mutualTuple = (parse(Int8, currentMutual[1]), parse(Int8, currentMutual[2]))
+        mutualTuple = (mutualTuple, parse(Float64, currentMutual[3]))
+        push!(mutualInd, mutualTuple)
     end
 
     for i in 1:numLoops                         #Asks about circuit elements
         push!(loops, [])
-        write(nl, ",Loop $(i-1)")               #Write each loop name as a header
-        write(lc, "Loop $(i-1)")
+        #write(lc, "Loop $(i-1)") #🟡
         println("Enter all components in Loop $(i-1) one by one\n(Enter '~' when all components are listed)")
         while true
             input = readline()
             if (input == "~")
-                write(lc, "\n")                 
+                #write(lc, "\n")#🟡                 
                 break
             end
             push!(loops[i], input)
-            write(lc, ",$input")                #Write each component to the loops+components.csv file
+            #write(lc, ",$input")#🟡                #Write each component to the loops+components.csv file
         end
     end
 
     for i in 1:numLoops                    #Iterate through all loops to find unique circuit elements      
         jj_count = 0
         for j in 1:length(loops[i])             #Iterate components in current loop
-            if (loops[i][j][1] == 'J')             #Count Josephson Junctions in current loop
+            if (loops[i][j][1] == 'J')             #Count Josephson Junctions in current loop 🔴
                 jj_count = jj_count + 1
             end
             componentLoopDict[loops[i][j]]=push!(get(componentLoopDict, loops[i][j], []), i-1) #Creates dict with unique circuit elements
         end
-        if (jj_count > 1)                       #If current loop has more than one Josephson Junction it is a SQUID loop... is this important?
+        if (jj_count > 1)                       #If current loop has more than one Josephson Junction it is a SQUID loop... is this important? 🔴
             push!(squidLoops, i-1)
         end
     end
@@ -96,72 +95,64 @@ function netlist(name)                  #USE JLD2.jl
     end
 
     for j in 1:numLoops                         #First iteration to find the self and mutal inductance of all loops
-        write(nl, "\nLoop $(j-1),/")
+        current_row = []
         for i in 2:numLoops                     #Second iteration to go through loop 1 to i for each and every loop 
     #                                           ^--- suspect matrix symmetry - better performance with "for i in j:numLoops"
-            write(nl, ",")
+            temp_float = 0.0
             #SELF COUPLING
             for n in loops[i]                   #Third iteration to go through the components in loop 1
                 if ((n[1] == 'J') || (n[1] == 'L')) #Merge if statemets??
                     if (j-1 in get(componentLoopDict, n, -1))   #If component n is in the loop j
                         if (i == j)             #Positive/Negative <--- Needs to be checked
-                            write(nl, "+")
+                            temp_float = temp_float + parse(Float64, get(componentParamDict, n, -1)[1])
                         else
-                            write(nl, "-")
+                            temp_float = temp_float - parse(Float64, get(componentParamDict, n, -1)[1])
                         end
-                        write(nl, "$n")         #Parse first to generate numerical value?
                     end
                 end
             end
             #MUTUAL COUPLING
             for n in mutualInd
                 if ((i != j) && (i-1 in n[1]) && (j-1 in n[1]))
-                    write(nl, "-$(n[2])")
+                    temp_float = temp_float - n[2]
                 end
             end
+            push!(current_row, temp_float)
         end 
+        L = [L; current_row']
     end
 
     for i in 1:length(junctions)
-        write(nl, "\nJunction $i")
+        current_row = []
         for j in 1:numLoops
             if junctions[i] in loops[j]
-                write(nl, ",1")
+                push!(current_row, 1)
             else
-                write(nl, ",0")
+                push!(current_row, 0)
             end
         end
+        σB = [σB; current_row']
     end
 
-    println("Enter the loops where external flux passes:\nE.g. if half the external flux passes through loop 1 and the remaining flux passes through loop 2 enter \n'1,0.5'\n'2,0.5'\n(Enter '~' when all are listed)")
-    while true
-        input = readline()
-        if (input == "~")           
-            break
-        end
-        input = split(input, ",")
-        flux = (parse(Int8, input[1]), parse(Float16, input[2]))    #Create tuple only works for single digit int
-        push!(extern_flux, flux)
+    println("Enter the external flux through each loop:\nE.g. if there are 3 loops (0, 1, 2) and 0.6 of the external flux passes through loop 1 and the remaining flux passes through loop 2 enter \n'0-0.6-0.4'")
+    input = readline()
+    flux = split(input, '-')
+    for i in 1:length(flux)
+        push!(extern_flux, parse(Float64, flux[i]))
     end
 
-    write(nl, "\nFlux Φ,/")
-    for i in 2:numLoops                                 #NEEDS WORK if external flux passes through two loops this does not work
-        for j in 1:length(extern_flux)
-            if (i-1 == extern_flux[j][1])
-                write(nl, ",$(extern_flux[j][2])")
-            else
-                write(nl, ",0")
-            end
-        end
+    L = transpose(L)
+    σA = transpose(σB[:, [2,end]])
+
+    jldopen("$(name).jld2", "w") do file
+        file["editing/loops"] = loops
+        file["editing/parameters"] = componentParamDict
+        file["matrices/L"] = L
+        file["matrices/σA"] = σA
+        file["matrices/σB"] = σB
+        file["matrices/k"] = extern_flux
     end
 
-    for (key, value) in componentParamDict
-        write(cp, "$key: $value\n")
-    end
-
-    close(nl)
-    close(cp)
-    close(lc)
 end
 
 netlist("test")
